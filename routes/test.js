54929833d0be576c3627cc0a5533001d0fd37d8c
Gospel011 <ugochukwugospeli111@gmail.com>
2023-10-07 13:31:44 +0100

    Added smiley faces

--- routes/test.js
+++ routes/test.js
@@ -6,7 +6,7 @@ router.get('/status', (req, res, next) => {
     console.log('TEST ROUTE')
     res.status(200).json({
         status: 'success',
-        message: 'Gospel is a divine monarch and Emmanuel Ndema is his imperial servant'
+        message: 'Gospel is a divine monarch and Emmanuel Ndema is his imperial servant 🤴😂'
     })
 })
 
