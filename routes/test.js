23b00c9e6ad7da4414e59e1e2afd0d3c797cb4b9
Gospel011 <ugochukwugospeli111@gmail.com>
2023-10-07 13:27:54 +0100

    RETOUCHED THE TEST MESSAGE

--- routes/test.js
+++ routes/test.js
@@ -6,7 +6,7 @@ router.get('/status', (req, res, next) => {
     console.log('TEST ROUTE')
     res.status(200).json({
         status: 'success',
-        message: 'App is running fine'
+        message: 'Gospel is a divine monarch and Emmanuel Ndema is his imperial servant'
     })
 })
 
