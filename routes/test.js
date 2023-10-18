4078ff522a2f7354eedcf2cbeef4006b39b127cb
Gospel011 <111324674+Gospel011@users.noreply.github.com>
2023-10-18 01:15:36 +0100

    Increased the fields size and the parts

--- routes/test.js
+++ routes/test.js
@@ -10,7 +10,8 @@ router.get('/status', (req, res, next) => {
             '1.)':  'Improved the auth route and made some changes to the user model',
             '2.)':  'Added keep alive functionality',
             '3.)':  'Increased KAT',
-            '4.)':  'Return user on sign in'
+            '4.)':  'Return user on sign in',
+            '5.)':  'Increased the fields size and the parts'
         }
     })
 })
